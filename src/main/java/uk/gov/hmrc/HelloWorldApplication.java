fcf8ac873abe338b0fd3ffc077a318ce43a993ad
criso <cristian.vladut@dvla.gov.uk>
2020-12-27 21:17:53 +0000

    update project to work

--- src/main/java/uk/gov/hmrc/HelloWorldApplication.java
+++ src/main/java/uk/gov/hmrc/HelloWorldApplication.java
@@ -19,7 +19,7 @@ package uk.gov.hmrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class HelloWorldApplication extends SpringBootServletInitializer {
